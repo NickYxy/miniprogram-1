c586325c41acb843546ee903e215b5d7ba42124a
NickYxy <bing900713@hotmail.com>
2024-05-30 17:25:20 +0800

    bindtap

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,13 @@
 Page({
 	data: {
-		info: 'Hello Nick'
-	}
+		info: 'Hello Nick',
+		//imgSrc: '/image/B-1.jpg',
+		randomNum1: Math.random().toFixed(2) * 10
+	},
+
+	btnTapHandler(e) {
+		console.log(e)
+	},
+
+	onLoad: function(options){}
 })
